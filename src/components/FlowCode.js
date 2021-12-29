f669e4b91dec9f6d2fbbced3d215a694f9ef875e
Arathurs <arathurs@gmail.com>
2021-12-29 00:01:49 -0500

    Remove err boundary and import of err boundary from FlowCode container

--- src/components/FlowCode.js
+++ src/components/FlowCode.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FlowCodeErrorBoundary } from './errors/FlowCodeErrorBoundary';
 import { FlowCodeHeadline } from './FlowCodeHeadline';
 import { ColorList } from './ColorList';
 
@@ -7,18 +6,10 @@ export function FlowCode(props) {
 	
 	return (
 		<div className="container">
-			
-			<FlowCodeErrorBoundary>
 				
 				<FlowCodeHeadline onClick={props.startOver} selectedColor={props.selectedColor} />
-			
-			</ FlowCodeErrorBoundary>
-			
-			<FlowCodeErrorBoundary>
 				
 				<ColorList changeColor={props.changeColor} colors={props.colors} hash={props.hash}/>
-			
-			</ FlowCodeErrorBoundary>
 		
 		</div>
 	);
